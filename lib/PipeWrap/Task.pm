854947eff755809b8b5411ab4eff4468394057e7
Thomas Hackl <thomas.hackl@uni-wuerzburg.de>
2014-07-14 17:56:29 +0200

    + PipeWarp::Task to wrap single tasks in objects

---
+++ lib/PipeWrap/Task.pm
@@ -0,0 +1,101 @@
+package PipeWrap::Task;
+
+use Log::Log4perl qw(:easy :no_extra_logdie_message);
+
+#-----------------------------------------------------------------------------#
+# Globals
+
+our $VERSION = '0.01';
+
+my $L = Log::Log4perl::get_logger();
+
+
+
+#-----------------------------------------------------------------------------#
+=head2 new
+
+=cut
+
+sub new{
+
+    $L->debug("initiating ".__PACKAGE__." object");
+
+    my $proto = shift;
+    my $self;
+    my $class;
+    
+    # object method -> clone + overwrite
+    if($class = ref $proto){ 
+	return bless ({%$proto, @_}, $class);
+    }
+
+    # class method -> construct + overwrite
+    # init empty obj
+    $self = {
+	id => '',
+	cmd => [],
+	parser => [],
+	# overwrite
+	@_,
+	# protected privates
+    };
+
+    bless $self, $proto;    
+
+    return $self;
+}
+
+
+# =head2 run
+
+# =cut
+
+
+##----------------------------------------------------------------------------##
+#Accessors
+
+=head2 cmd
+
+Get/set command.
+
+=cut
+
+sub cmd{
+    my ($self, $cmd, $force) = @_;
+    if(defined($cmd) || $force){
+	$self->{cmd} = $cmd;
+    }
+    return $self->{cmd};
+}
+
+
+=head2 id
+
+Get/set id.
+
+=cut
+
+sub id{
+    my ($self, $id, $force) = @_;
+    if(defined($id) || $force){
+	$self->{id} = $id;
+    }
+    return $self->{id};
+}
+
+=head2 parser
+
+Get/set result parser
+
+=cut
+
+sub parser{
+    my ($self, $parser, $force) = @_;
+    if(defined($parser) || $force){
+	$self->{parser} = $parser;
+    }
+    return $self->{parser};
+}
+
+1;
+
