8b9e9f7a8fcb28c6560fc5e07a053e9434a33a7d
JReins <aaron.sigmund@stud-mail.uni-wuerzburg.de>
2016-11-16 14:11:53 +0100

    updated doc

--- PipeWrap/lib/PipeWrap.pm
+++ PipeWrap/lib/PipeWrap.pm
@@ -17,40 +17,62 @@ __END__
 
 =head1 NAME
 
-PipeWrap - Perl extension for blah blah blah
+PipeWrap - Checkpoint system for perl modules.
+Used in chloroExtractor, a scientific tool for extraction of chloroplast dna out of a whole plant genome.
 
 =head1 SYNOPSIS
 
   use PipeWrap;
-  blah blah blah
 
 =head1 DESCRIPTION
 
-Stub documentation for PipeWrap, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
+Checkpoint system that supervises a given order of tasks given by a config file.
 
-Blah blah blah.
+=head2 new
 
+PipeWrap::new();
 
-=head1 SEE ALSO
+new() creates PipeWrap object with given options from config file
 
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
+=cut
 
-If you have a mailing list set up for your module, mention it here.
+=head2 bless_tasks
 
-If you have a web site set up for your module, mention it here.
+$new->bless_tasks();
+
+bless_tasks() blesses tasks in PipeWrap object
+
+=cut
+
+=head2 index_tasks
+
+$new->index_tasks();
+
+index_tasks() indicates all given tasks
+
+=cut
+
+=head2 run
+
+$new->run();
+
+run() 
+
+=head1 SEE ALSO                                                                                      
+Mention other useful documentation such as the documentation of                                      
+related modules or operating system documentation (such as man pages                                 
+documentation such as RFCs or                                                                       
+standards.  
 
 =head1 AUTHOR
 
-Aaron Sigmund, E<lt>s222434@E<gt>
+Thomas Hackl, E<lt>mail<gt>
+Simon Pfaff, E<lt>simon.pfaff@stud-mail.uni-wuerzburg.de<gt>
+Aaron Sigmund, E<lt>aaron.sigmund@stud-mail.uni-wuerzburg.de<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2016 by Aaron Sigmund
+Copyright (C) 2016 by CCTB UniWuerzburg
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.18.2 or,
