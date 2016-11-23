3844f40ea456516449376b9334fe590f2b6878c1
JReins <aaron.sigmund@stud-mail.uni-wuerzburg.de>
2016-11-23 16:50:02 +0100

    added methods in Task (run, parse_raw, parse_csv)

--- lib/PipeWrap/Task.pm
+++ lib/PipeWrap/Task.pm
@@ -56,6 +56,32 @@ gets tasks from object and calls corresponding command
 
 =cut
 
+sub run {
+    my ($self) = @_;
+    # run task
+    open(my $cmdh, "@{$self->cmd()} |") or $L->logdie($!);
+    
+    # retrieve results
+    my $re;
+
+    my $parser = $self->parser || "parse_raw";
+    if(ref $parser eq 'CODE'){
+	$L->debug("Using custom parser routine, returned");
+	$re = &{$parser}($cmdh);
+	$L->debug(Dumper($re));
+    }else{
+	$L->debug("Using predefined parser");
+	$re = $self->$parser($cmdh);
+    }
+    
+    close $cmdh;
+    $L->logcroak($self->id." exited:$? $@\n", $re) if ($? || $@);
+    
+    $L->debug($self->id."returned:\n",  ref $re ? Dumper($re) : $re);
+
+    return $re;
+}
+
 =head2 parse_raw
 
 $new->parse_raw($FH)
@@ -63,12 +89,28 @@ simple parser that reads the entire output of a FH to a string
 
 =cut
 
+sub parse_raw {
+    my ($self, $fh) = @_;
+    return scalar do{local $/; <$fh>}
+}
+
 =head2 parse_csv
 
 $new->parse_csv($FH)
 simple parser that splits whitespace separated output into a hash
 
 =cut
+
+sub parse_csv {
+    my ($self, $fh) = @_;
+    my %re;
+    while(<$fh>){
+        my ($k, $v) = split(/\s+/, $_, 2);
+	chomp($v);
+        $re{$k} = $v;
+    }
+    return \%re;
+}
 
 =head1 ACCESSORS
 =cut
